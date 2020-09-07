8d49b57de7021e69a6b957dd219b123ced438446
David <david10@davidjpotter.com>
2020-09-06 19:04:12 -0700

    misc

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function Home() {
-  return <h1>Home</h1>;
-}
+const useStyles = makeStyles((theme) => ({
+  root: { paddingTop: theme.spacing(3), minHeight: 500 },
+}));
+const Home = () => {
+  const classes = useStyles();
+  const [agreement, setAgreement] = useState("");
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg" className={classes.root}>
+        <Typography
+          component="h1"
+          variant="h2"
+          color="textPrimary"
+          className={classes.pageTitle}
+        >
+          Home
+        </Typography>
+        <p>{agreement.contract}</p>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default Home;
