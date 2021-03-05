2c978350e630dc5215d8841fbbcd3b1e37b2fed7
tawhidabdullah <tawhidabdullah22@gmail.com>
2021-03-05 16:18:26 +0600

    CLIN-29, TEST.PNG => Developing UI for test graph ---- Updates on reviewed PR comments

--- client/src/screens/TestGraph/AxisBottom.js
+++ client/src/screens/TestGraph/AxisBottom.js
@@ -1,21 +1,37 @@
 import React from "react";
 
+import {
+  makeStyles,
+} from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  tickline: {
+    stroke: "#c0c0bb",
+  },
+  tickLabel: {
+    fill: "#635f5d",
+    textAnchor: "middle",
+  },
+}));
+
 export const AxisBottom = (
   {
     xScale, innerHeight,
     tickFormat, tickOffset = 3,
   },
-) => xScale.ticks().map((tickValue, i) => (
-  <g
-    className="tick"
-    key={tickValue}
-    transform={`translate(${xScale(tickValue)},0)`}
-  >
-    {i === 0 && <line y1={innerHeight} />}
-    <line y1={innerHeight} y2={innerHeight + 10} />
+) => {
+  const classes = useStyles();
+  return xScale.ticks().map((tickValue, i) => (
+    <g
+      key={tickValue}
+      transform={`translate(${xScale(tickValue)},0)`}
+    >
+      {i === 0 && <line y1={innerHeight} />}
+      <line y1={innerHeight} y2={innerHeight + 10} className={classes.tickline} />
 
-    <text style={{ textAnchor: "middle" }} dy="1.3em" y={innerHeight + tickOffset}>
-      {tickFormat(tickValue)}
-    </text>
-  </g>
-));
+      <text dy="1.3em" y={innerHeight + tickOffset} className={classes.tickLabel}>
+        {tickFormat(tickValue)}
+      </text>
+    </g>
+  ));
+};
--- client/src/screens/TestGraph/Axisleft.js
+++ client/src/screens/TestGraph/Axisleft.js
@@ -1,16 +1,34 @@
 import React from "react";
 
-export const AxisLeft = ({ yScale, innerWidth, tickOffset = 3 }) => yScale.ticks().map((tickValue, i) => (
-  <g className="tick" transform={`translate(0,${yScale(tickValue)})`}>
-    {i === 0 && <line x2={innerWidth} />}
-    <line x1={0} x2={5} />
-    <text
-      key={tickValue}
-      style={{ textAnchor: "end" }}
-      x={-tickOffset}
-      dy=".30em"
-    >
-      {tickValue}
-    </text>
-  </g>
-));
+import {
+  makeStyles,
+} from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  tickline: {
+    stroke: "#c0c0bb",
+  },
+  tickLabel: {
+    fill: "#635f5d",
+    textAnchor: "end",
+  },
+}));
+
+export const AxisLeft = ({ yScale, innerWidth, tickOffset = 3 }) => {
+  const classes = useStyles();
+  return yScale.ticks().map((tickValue, i) => (
+    <g className="tick" transform={`translate(0,${yScale(tickValue)})`}>
+      {i === 0 && <line x2={innerWidth} />}
+      <line x1={0} x2={5} className={classes.tickline} />
+      <text
+        key={tickValue}
+        style={{ textAnchor: "end" }}
+        x={-tickOffset}
+        dy=".30em"
+        className={classes.tickLabel}
+      >
+        {tickValue}
+      </text>
+    </g>
+  ));
+};
